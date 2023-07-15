4aedf04281fcda4a147dd11dd1a9a4658dc02fbc
2547techno <technowizz2547@gmail.com>
2023-07-15 00:51:53 -0400

    enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Express from "express";
+import cors from "cors";
 import { routes } from "./routes/routes";
 const app = Express();
 
 const PORT = process.env.PORT ?? 8080;
 
+app.use(cors());
 app.use(routes);
 
 app.listen(PORT, () => {
