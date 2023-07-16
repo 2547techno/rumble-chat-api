74a19c718edfcceeaf3739d83622786c92d2ae32
2547techno <technowizz2547@gmail.com>
2023-07-16 03:49:59 -0400

    rename metric prefix service_ to rumble_api_

--- src/lib/prometheus.ts
+++ src/lib/prometheus.ts
@@ -1,18 +1,18 @@
 import { Gauge } from "prom-client";
 
 const rumbleConnections = new Gauge({
-    name: "service_rumble_connections_total",
+    name: "rumble_api_rumble_connections_total",
     help: "Number of Rumble chat streams connected to",
 });
 
 const clientConnections = new Gauge({
-    name: "service_client_connections_total",
+    name: "rumble_api_client_connections_total",
     help: "Number of clients connected to service per stream",
     labelNames: ["sid"],
 });
 
 const clientConnectionsSum = new Gauge({
-    name: "service_client_connections_sum_total",
+    name: "rumble_api_client_connections_sum_total",
     help: "Number of clients connected to service total",
 });
 
